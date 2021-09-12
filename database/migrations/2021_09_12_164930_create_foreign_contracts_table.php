474ce2712fff061c28d0af33b4ba9ca4e1a83bb4
Eugene Dmitrievskiy <ravenio@ex.ua>
2021-09-12 20:07:39 +0300

    contracts and insurance programs migrations

---
+++ database/migrations/2021_09_12_164930_create_foreign_contracts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForeignContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foreign_contracts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('territory_covered')->nullable();
+            $table->dateTime('valid_from')->nullable();
+            $table->dateTime('valid_till')->nullable();
+            $table->integer('duration')->nullable();
+            $table->foreignId('risk_group');
+            $table->string('insured');
+            $table->string('passport');
+            $table->date('birth_date');
+            $table->integer('franchise');
+            $table->foreignId('insurance_program');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foreign_contracts');
+    }
+}
