2daed2ce231c08f57b0759d5496ef87e57487203
Eugene Dmitrievskiy <ravenio@ex.ua>
2021-09-13 17:58:10 +0300

    contracts controller and views skeleton

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,13 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="divide-y divide-gray-200">
-                    <p class="p-6 bg-white border-b border-gray-200">
+                    <p class="p-6 bg-white border-b border-gray-200 text-xl font-medium text-black">
                         Сгенерировать договор
                     </p>
                 </div>
-                <div class="p-6">
-                    Сгенерировать договор
-                </div>
+
+                <form class="p-6 space-y-6" method="post">
+{{--                    <div>--}}
+{{--                        <label for="name" class="block">1.Им'я особи</label>--}}
+{{--                    </div>--}}
+                    <div class="mt-1">
+{{--                        <input type="text" id="name" name="name" class="w-full border border-gray-300 px-3 py-2--}}
+{{--                        rounded-lg shadow-sm"--}}
+{{--                               required>--}}
+                        <x-label for="name" :value="__('Ім\'я')" />
+                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                        <div class="mt-4">
+                            <x-label for="last_name" :value="__('Фамілія')" />
+
+                            <x-input id="password" class="block mt-1 w-full"
+                                     type="password"
+                                     name="password"
+                                     required autocomplete="current-password" />
+                        </div>
+                    </div>
+                </form>
+
             </div>
         </div>
     </div>
