96e5c73605f5e322f1832894885622d81e94c7ed
Eugene Dmitrievskiy <ravenio@ex.ua>
2021-09-15 15:44:28 +0300

    foreign contract includes

---
+++ resources/views/contracts/PDF/parts/body.blade.php
@@ -0,0 +1,145 @@
+<table class="text-8" style="border: 1px solid black">
+    <tbody>
+    <tr>
+        <td style="border: 1px solid black;">Територія дії</td>
+        <td rowspan="2"
+            style="border: 1px solid black;">
+            Європа/Europe
+        </td>
+        <td style="border: 1px solid black;">Строк дії</td>
+        <td rowspan="2"
+            style=" border: 1px solid black; width: 100px">
+            19/09/2021
+        </td>
+        <td style="border: 1px solid black;">По</td>
+        <td rowspan="2"
+            style="border: 1px solid black; ">
+            19/09/2022
+        </td>
+        <td style="border: 1px solid black;">Кількість днів</td>
+        <td rowspan="2"
+            style="border: 1px solid black; ">
+            90
+        </td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black;">Territory covered</td>
+        <td style="border: 1px solid black;">Valid from</td>
+        <td style="border: 1px solid black;">Till</td>
+        <td style="border: 1px solid black;">Duration (days)</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black;">Програма страхування</td>
+        <td rowspan="2"
+            style="border: 1px solid black;">
+            A
+        </td>
+        <td rowspan="2"
+            colspan="3"
+            style="border: 1px solid black;">
+            Група ризику/ Risk group
+        </td>
+        <td style="border: 1px solid black;">Робота
+        </td>
+        <td style="border: 1px solid black;">
+            Навчання
+        </td>
+        <td style="border: 1px solid black;">Спорт</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black;">Insurance programme</td>
+        <td style="border: 1px solid black;">X</td>
+        <td style="border: 1px solid black;"></td>
+        <td style="border: 1px solid black;"></td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black;">Cтрахувальник / Insured</td>
+        <td style="border: 1px solid black;">Дані паспорту /Passport</td>
+        <td style="border: 1px solid black;">Дата народження/Birth date</td>
+        <td style="border: 1px solid black;">Знижки/надбавки(%) </td>
+        <td rowspan="2" style="border: 1px solid black;">10</td>
+        <td colspan="2" style="border: 1px solid black;">Франшиза</td>
+        <td  rowspan="2" style="border: 1px solid black;">50</td>
+    </tr>
+    <tr>
+        <td rowspan="2" style="border: 1px solid black;">Chereda Olelsandr</td>
+        <td rowspan="2" style="border: 1px solid black;">EK419790</td>
+        <td rowspan="2" style="border: 1px solid black;">03/19/1977</td>
+        <td style="border: 1px solid black;">Discount/Allowance(%)</td>
+        <td colspan="2" style="border: 1px solid black;">Deductible</td>
+    </tr>
+    <tr>
+        <td  style="border: 1px solid black;">Кількість осіб </td>
+        <td rowspan="3" style="border: 1px solid black;">1</td>
+        <td colspan="2" style="border: 1px solid black">Страхова сума на одну особу</td>
+        <td style="border: 1px solid black;">Страхова премія на одну особу</td>
+    </tr>
+    <tr>
+
+        <td style="border: 1px solid black; ">Адреса, телефон</td>
+        <td rowspan="2" colspan="2" style="border: 1px solid black;">Kiev</td>
+        <td rowspan="2" style="border: 1px solid black;">Persons</td>
+        <td rowspan="2" colspan="2" style="border: 1px solid black;">Amount insured per
+            person</td>
+        <td rowspan="2" style="border: 1px solid black;">Insurance premium per person</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black; ">Address, phone </td>
+    </tr>
+    <tr>
+        <td colspan="3" style="border: 1px solid black; ">Застраховані особи/Insured persons</td>
+        <td colspan="2" style="border: 1px solid black; ">Медичні витрати</td>
+        <td  colspan="2" rowspan="2" style="border: 1px solid black; ">30000 EUR</td>
+        <td rowspan="2" style="border: 1px solid black; ">100.00</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black; ">Chereda Olelsandr</td>
+        <td style="border: 1px solid black; ">EK419790</td>
+        <td style="border: 1px solid black; ">03/19/1977</td>
+        <td colspan="2"  style="border: 1px solid black; ">Medical expenses</td>
+    </tr>
+    <tr>
+        <td rowspan="2" style="border: 1px solid black; ">&nbsp;</td>
+        <td rowspan="2" style="border: 1px solid black; ">&nbsp;</td>
+        <td rowspan="2" style="border: 1px solid black; ">&nbsp;</td>
+        <td colspan="2" style="border: 1px solid black; ">Від нещасних випадків (грн.)</td>
+        <td  colspan="2" rowspan="2" style="border: 1px solid black; ">10000 EUR</td>
+        <td rowspan="2" style="border: 1px solid black; ">10.00</td>
+    </tr>
+    <tr>
+        <td colspan="2" style="border: 1px solid black; ">Accident insurance (UAH)</td>
+    </tr>
+    <tr>
+        <td  style="border: 1px solid black; ">&nbsp;</td>
+        <td  style="border: 1px solid black; ">&nbsp;</td>
+        <td  style="border: 1px solid black; ">&nbsp;</td>
+        <td colspan="2" style="border: 1px solid black;">Цивільна відповідальність/Public liability</td>
+        <td  colspan="2" style="border: 1px solid black;">30000 EUR</td>
+        <td  style="border: 1px solid black;">100.00</td>
+    </tr>
+    <tr>
+        <td  style="border: 1px solid black; ">&nbsp;</td>
+        <td  style="border: 1px solid black; ">&nbsp;</td>
+        <td  style="border: 1px solid black; ">&nbsp;</td>
+        <td colspan="4" style="border: 1px solid black; ">Загальна страхова премія (грн.)/Total bonus (UAH)
+        </td>
+        <td style="border: 1px solid black; ">210.00</td>
+    </tr>
+    <tr>
+        <td rowspan="2" style="border: 1px solid black; ">Вигодонабувач/Beneficiary</td>
+        <td rowspan="2" colspan="2" style="border: 1px solid black; ">&nbsp;</td>
+        <td colspan="4" style="border: 1px solid black; ">Дата видачі / Issue date</td>
+        <td  style="border: 1px solid black; ">08/02/2016</td>
+    </tr>
+    <tr>
+        <td colspan="4" style="border: 1px solid black; ">Агент</td>
+        <td style="border: 1px solid black; ">&nbsp;</td>
+    </tr>
+    <tr>
+        <td colspan="3" style="border: 1px solid black; ">З правилами страхування ознайомлені та згодні.</td>
+        <td colspan="3" style="border: 1px solid black; ">Підпис Страхувальника</td>
+        <td colspan="2" style="border: 1px solid black; ">Підпис Страховика </td>
+    </tr>
+
+    </tbody>
+</table>
