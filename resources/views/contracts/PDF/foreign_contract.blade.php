ab2f33e6c4ee3a63f366597e8e804b12a96f881b
Eugene Dmitrievskiy <ravenio@ex.ua>
2021-09-15 15:26:54 +0300

    foreign contract template finished

--- resources/views/contracts/PDF/foreign_contract.blade.php
+++ resources/views/contracts/PDF/foreign_contract.blade.php
@@ -76,29 +76,81 @@
             </tr>
             <tr>
                 <td  style="border: 1px solid black;">Кількість осіб </td>
-                <td rowspan="2" style="border: 1px solid black;">1</td>
+                <td rowspan="3" style="border: 1px solid black;">1</td>
                 <td colspan="2" style="border: 1px solid black">Страхова сума на одну особу</td>
                 <td style="border: 1px solid black;">Страхова премія на одну особу</td>
             </tr>
             <tr>
 
                                 <td style="border: 1px solid black; ">Адреса, телефон</td>
-                                <td rowspan="2" style="border: 1px solid black;">Kiev</td>
+                                <td rowspan="2" colspan="2" style="border: 1px solid black;">Kiev</td>
                                 <td rowspan="2" style="border: 1px solid black;">Persons</td>
-                                <td rowspan="2" style="border: 1px solid black;">1</td>
-                                <td colspan="2" style="border: 1px solid black;">Amount insured per person</td>
-                                <td style="border: 1px solid black;">Insurance premium per person</td>
+                                <td rowspan="2" colspan="2" style="border: 1px solid black;">Amount insured per
+                                    person</td>
+                                <td rowspan="2" style="border: 1px solid black;">Insurance premium per person</td>
             </tr>
             <tr>
                 <td style="border: 1px solid black; ">Address, phone </td>
             </tr>
+            <tr>
+                <td colspan="3" style="border: 1px solid black; ">Застраховані особи/Insured persons</td>
+                <td colspan="2" style="border: 1px solid black; ">Медичні витрати</td>
+                <td  colspan="2" rowspan="2" style="border: 1px solid black; ">30000 EUR</td>
+                <td rowspan="2" style="border: 1px solid black; ">100.00</td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid black; ">Chereda Olelsandr</td>
+                <td style="border: 1px solid black; ">EK419790</td>
+                <td style="border: 1px solid black; ">03/19/1977</td>
+                <td colspan="2"  style="border: 1px solid black; ">Medical expenses</td>
+            </tr>
+            <tr>
+                <td rowspan="2" style="border: 1px solid black; ">&nbsp;</td>
+                <td rowspan="2" style="border: 1px solid black; ">&nbsp;</td>
+                <td rowspan="2" style="border: 1px solid black; ">&nbsp;</td>
+                <td colspan="2" style="border: 1px solid black; ">Від нещасних випадків (грн.)</td>
+                <td  colspan="2" rowspan="2" style="border: 1px solid black; ">10000 EUR</td>
+                <td rowspan="2" style="border: 1px solid black; ">10.00</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="border: 1px solid black; ">Accident insurance (UAH)</td>
+            </tr>
+            <tr>
+                <td  style="border: 1px solid black; ">&nbsp;</td>
+                <td  style="border: 1px solid black; ">&nbsp;</td>
+                <td  style="border: 1px solid black; ">&nbsp;</td>
+                <td colspan="2" style="border: 1px solid black; ">Цивільна відповідальність/Public liability</td>
+                <td  colspan="2" style="border: 1px solid black; ">30000 EUR</td>
+                <td  style="border: 1px solid black; ">100.00</td>
+            </tr>
+            <tr>
+                <td  style="border: 1px solid black; ">&nbsp;</td>
+                <td  style="border: 1px solid black; ">&nbsp;</td>
+                <td  style="border: 1px solid black; ">&nbsp;</td>
+                <td colspan="4" style="border: 1px solid black; ">Загальна страхова премія (грн.)/Total bonus (UAH)
+                </td>
+                <td style="border: 1px solid black; ">210.00</td>
+            </tr>
+            <tr>
+                <td rowspan="2" style="border: 1px solid black; ">Вигодонабувач/Beneficiary</td>
+                <td rowspan="2" colspan="2" style="border: 1px solid black; ">&nbsp;</td>
+                <td colspan="4" style="border: 1px solid black; ">Дата видачі / Issue date</td>
+                <td  style="border: 1px solid black; ">08/02/2016</td>
+            </tr>
+            <tr>
+                <td colspan="4" style="border: 1px solid black; ">Агент</td>
+                <td style="border: 1px solid black; ">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="border: 1px solid black; ">З правилами страхування ознайомлені та згодні.</td>
+                <td colspan="3" style="border: 1px solid black; ">Підпис Страхувальника</td>
+                <td colspan="2" style="border: 1px solid black; ">Підпис Страховика </td>
+            </tr>
+
             </tbody>
         </table>
         <p style="margin-top: 5px">ТОВАРИСТВО З ДОДАТКОВОЮ ВІДПОВІДАЛЬНІСТЮ «СТРАХОВА КОМПАНІЯ «БЕНЄФІТ»  Адреса:
-            Україна,
-            030262,
-            м. Київ,
-            Столичне шосе 101 В,   тел. 0 800 33-05-54 Insurer: ADDITIONAL LIABILITY COMPANY «INSURANCE COMPANY «BENEFIT» Координати Контакт центра: +38 044 594 27 94 (мова спілкування українська, російська, англійська)</p>
+            Україна,030262, м. Київ, Столичне шосе 101 В,   тел. 0 800 33-05-54 Insurer: ADDITIONAL LIABILITY COMPANY «INSURANCE COMPANY «BENEFIT» Координати Контакт центра: +38 044 594 27 94 (мова спілкування українська, російська, англійська)</p>
         <hr>
         @include('contracts.PDF.parts.header')
 
